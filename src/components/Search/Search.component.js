97fe3748c10b056490eabd2be8363f0eb01ad3f0
Muhammad Ismail <yahyaismail@gmail.com>
2021-10-27 15:24:57 -0400

    Add filter components with mobile styling

--- src/components/Search/Search.component.js
+++ src/components/Search/Search.component.js
@@ -13,6 +13,8 @@ import "./Search.styles.scss";
 
 export const Search = () => {
   const [year, setYear] = useState("");
+  const [make, setMake] = useState("");
+  const [type, setType] = useState("");
 
   const handleChange = (event) => {
     setYear(event.target.value);
@@ -24,30 +26,63 @@ export const Search = () => {
         id='input-with-icon-adornment'
         placeholder='Search by Make or Model'
         className='search__input'
-        disableUnderline
+        // disableUnderline
         startAdornment={
           <InputAdornment position='start'>
             <SearchIcon className='search__icon' />
           </InputAdornment>
         }
       />
-
-      <FormControl className='filter'>
-        <InputLabel id='select-year-label' className='filter__select-label'>
-          Year
-        </InputLabel>
-        <Select
-          className='filter__select'
-          labelId='select-year-label'
-          id='select-year'
-          value={year}
-          label='Year'
-          onChange={handleChange}>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-      </FormControl>
+      <div className='filter'>
+        <FormControl>
+          <InputLabel id='select-year-label' className='filter__select-label'>
+            Year
+          </InputLabel>
+          <Select
+            className='filter__select'
+            labelId='select-year-label'
+            id='select-year'
+            value={year}
+            label='Year'
+            onChange={handleChange}>
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl>
+          <InputLabel id='select-make-label' className='filter__select-label'>
+            Make
+          </InputLabel>
+          <Select
+            className='filter__select'
+            labelId='select-make-label'
+            id='select-year'
+            value={make}
+            label='Make'
+            onChange={handleChange}>
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl>
+          <InputLabel id='select-type-label' className='filter__select-label'>
+            Type
+          </InputLabel>
+          <Select
+            className='filter__select'
+            labelId='select-type-label'
+            id='select-year'
+            value={type}
+            label='Type'
+            onChange={handleChange}>
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 };
